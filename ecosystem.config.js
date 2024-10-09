1903ce80724365515781f86d477500d05bcaa282
Iqoneo6 <142778317+Iqoneo6@users.noreply.github.com>
2024-10-09 14:53:39 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "app.js",
+      script: "server.js",
       env: {
         NODE_ENV: "production",
         // Add other environment variables here

737d36e8137403b5a241ad32fe5566ee79c93cc2
Iqoneo6 <142778317+Iqoneo6@users.noreply.github.com>
2024-10-10 15:30:13 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: './server.js',     // Path to the main entry point of your application
       instances: 'max',          // Use all available CPU cores
       exec_mode: 'cluster',      // Run in cluster mode
-      watch: true,               // Enable watch mode for automatic restarts on file changes
+      // watch: true,               // Enable watch mode for automatic restarts on file changes
       max_memory_restart: '1G',  // Restart if memory usage exceeds 1GB
       env: {
         NODE_ENV: 'development', // Environment variables for development

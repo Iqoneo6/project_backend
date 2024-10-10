de1eb128910b60ae0d2f99274f59064a04b814c0
Iqoneo6 <sachinvel59@gmail.com>
2024-10-10 14:15:10 +0530

    Thisis3commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'my-web-app',
-      script: './server.js',
-      instances: 'max',
-      exec_mode: 'cluster',
-      watch: false,
+      name: 'my-node-app',       // Name of your application
+      script: './server.js',     // Path to the main entry point of your application
+      instances: 'max',          // Use all available CPU cores
+      exec_mode: 'cluster',      // Run in cluster mode
+      watch: true,               // Enable watch mode for automatic restarts on file changes
+      max_memory_restart: '1G',  // Restart if memory usage exceeds 1GB
       env: {
-        NODE_ENV: 'development',
-        PORT: 3000,
+        NODE_ENV: 'development', // Environment variables for development
+        PORT: 3000,              // Development port
       },
       env_production: {
-        NODE_ENV: 'production',
-        PORT: 8000,
+        NODE_ENV: 'production',   // Environment variables for production
+        PORT: 8000,               // Production port
       },
     },
   ],

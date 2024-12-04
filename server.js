4a5f947c1c1d7154e171393a4f93f3e5d9e05946
iqooneo6 <sachinvel59@gmail.com>
2024-12-04 14:53:50 +0530

    This is Ci and Cd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.status(200).json({
     message:
-      "Hi All,server is up and running.",
+      "Hi All,I am server.",
   });
 });
 const {PORT, NODE_ENV } = process.env;

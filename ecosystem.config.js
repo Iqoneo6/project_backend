09c190ed058a6f1058fca8ac9dc6e82b00306ebe
Iqoneo6 <142778317+Iqoneo6@users.noreply.github.com>
2024-12-06 16:23:21 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,42 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "Project_backend",
+      name: "Project_Backend",
       script: "server.js",
-      // watch: true,
-      node_args: "--max_old_space_size=16000",
-      log: "./log/combined.outerr0.log",
-      output: "./log/pm2/out.log",
-      error: "./log/pm2/error.log",
-      ignore_watch: [
-        "log",
-        "log/",
-        ".node-gyp",
-        ".node-gyp/",
-        ".pm2",
-        ".pm2/",
-        "public",
-        "public/",
-        "xml_file/*",
-      ],
-      env: {
-        NODE_ENV: "production", // Note: "env" instead of "env_prod"
-        PORT:5000,
-        exec_mode : "cluster"
-
-      },
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      watch: true,
     },
   ],
-  logrotate: {
-    enabled: true,
-    // Rotate logs daily
-    rotateInterval: "0 0   *",
-    // Keep 30 days of logs
-    retain: "30",
-    // Log files path
-    filePath: "log/pm2",
-    // Compress rotated logs
-    compress: true,
-  },
 };
